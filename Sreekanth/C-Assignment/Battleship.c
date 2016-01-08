dba0523637975c1a6ce306a3a24f14fe7e06e2c0
vjsreekanth <vjsreekanth@gmail.com>
2016-01-07 16:42:07 -0800

    ships completed

--- Sreekanth/C-Assignment/Battleship.c
+++ Sreekanth/C-Assignment/Battleship.c
@@ -35,6 +35,7 @@ typedef char orientation;
 void createMonsterShip(matrix_byte m[SIZE][SIZE]);
 void createBigShips(matrix_byte m[SIZE][SIZE]);
 void createMediumShips(matrix_byte m[SIZE][SIZE]);
+void createSmallShips(matrix_byte m[SIZE][SIZE]);
 
 
 int main()
@@ -42,13 +43,16 @@ int main()
 	matrix_byte matrix[SIZE][SIZE] = {WATER_VALUE};
  	createMonsterShip(matrix); 
  	createBigShips(matrix); 
- 	createMediumShips(matrix); 
+ 	createMediumShips(matrix);
+ 	createSmallShips(matrix); 
 
  	for(int i = 0; i < SIZE; i++)
  	{
  		for(int j = 0; j < SIZE; j++)
  		{
  			printf("%i ", matrix[i][j]);
+ 			if (matrix[i][j] != SMALL_VALUE_E)
+ 				printf(" ");
  		}
 
  		printf("\n");
@@ -92,6 +96,8 @@ void createMonsterShip(matrix_byte m[SIZE][SIZE])
 
 void createBigShips(matrix_byte m[SIZE][SIZE])
 {
+	// BIGSHIP A
+
 	int BigShipAX = -1;
 	int BigShipAY = -1;
 
@@ -114,32 +120,36 @@ void createBigShips(matrix_byte m[SIZE][SIZE])
 		break;
 	}
 
+	// BIGSHIP B
+
 	int BigShipBX = -1;
 	int BigShipBY = -1;
 
 	while(true)
 	{
 
-	BigShipBX = rand() % (SIZE - BIG_SIZE + 1);
-	BigShipBY = rand() % (SIZE);
+		BigShipBX = rand() % (SIZE - BIG_SIZE + 1);
+		BigShipBY = rand() % (SIZE);
 
-	if (m[BigShipBX][BigShipBY] != WATER_VALUE || 
-		m[BigShipBX + 1][BigShipBY] != WATER_VALUE ||
-		m[BigShipBX + 2][BigShipBY] != WATER_VALUE)
-	{
-		continue;
-	}
+		if (m[BigShipBX][BigShipBY] != WATER_VALUE || 
+			m[BigShipBX + 1][BigShipBY] != WATER_VALUE ||
+			m[BigShipBX + 2][BigShipBY] != WATER_VALUE)
+		{
+			continue;
+		}
 
-    m[BigShipBX][BigShipBY] = BIG_VALUE_B;
-	m[BigShipBX + 1][BigShipBY] = BIG_VALUE_B;
-	m[BigShipBX + 2][BigShipBY] = BIG_VALUE_B;
-	break;
+	    m[BigShipBX][BigShipBY] = BIG_VALUE_B;
+		m[BigShipBX + 1][BigShipBY] = BIG_VALUE_B;
+		m[BigShipBX + 2][BigShipBY] = BIG_VALUE_B;
+		break;
 
 	}
 }
 
 void createMediumShips(matrix_byte m[SIZE][SIZE])
 {
+	// MediumShip A
+
 	int mediumShipAX = -1;
 	int mediumShipAY = -1;
 
@@ -154,11 +164,11 @@ void createMediumShips(matrix_byte m[SIZE][SIZE])
 		continue;
 	}
 
-	
 	m[mediumShipAX][mediumShipAY] = MEDIUM_VALUE_A;
 	m[mediumShipAX][mediumShipAY + 1] = MEDIUM_VALUE_A;
 	break;
 	}
+	// MediumShip B
 
 	int mediumShipBX = -1;
 	int mediumShipBY = -1;
@@ -173,12 +183,123 @@ void createMediumShips(matrix_byte m[SIZE][SIZE])
 		continue;
 	}
 
-	
-
 	m[mediumShipBX][mediumShipBY] = MEDIUM_VALUE_B;
 	m[mediumShipBX][mediumShipBY + 1] = MEDIUM_VALUE_B;
 	break;
 	}
+}
+
+void createSmallShips(matrix_byte m[SIZE][SIZE])
+{
+	//SmallShip a
+
+	int smallShipAX = -1;
+	int smallShipAY = -1;
+
+	while(true)
+	{
+		smallShipAX = rand() % (SIZE);
+		smallShipAY = rand() % (SIZE);
+
+		if (m[smallShipAX][smallShipAY] != WATER_VALUE)
+		{
+			continue;
+		}	
+
+		m[smallShipAX][smallShipAY] = SMALL_VALUE_A;
+		
+		break;
+	}
+
+	//SmallShip B
+
+	int smallShipBX = -1;
+	int smallShipBY = -1;
+
+	while(true)
+	{
+		smallShipBX = rand() % (SIZE);
+		smallShipBY = rand() % (SIZE);
+
+		if (m[smallShipBX][smallShipBY] != WATER_VALUE)
+		{
+			continue;
+		}
+
+		
+
+		m[smallShipBX][smallShipBY] = SMALL_VALUE_B;
+		
+		break;
+	}
+
+	//SmallShip C
+
+	int smallShipCX = -1;
+	int smallShipCY = -1;
+
+	while(true)
+	{
+		smallShipCX = rand() % (SIZE);
+		smallShipCY = rand() % (SIZE);
+
+	if (m[smallShipCX][smallShipCY] != WATER_VALUE)
+	{
+		continue;
+	}
+
+	
+
+	m[smallShipCX][smallShipCY] = SMALL_VALUE_C;
+	
+	break;
+	}
+
+	//SmallShip D
+
+	int smallShipDX = -1;
+	int smallShipDY = -1;
+
+	while(true)
+	{
+		smallShipDX = rand() % (SIZE);
+		smallShipDY = rand() % (SIZE);
+
+	if (m[smallShipDX][smallShipDY] != WATER_VALUE)
+	{
+		continue;
+	}
+
+	
+
+	m[smallShipDX][smallShipDY] = SMALL_VALUE_D;
+	
+	break;
+	}
+
+	//SmallShip E
+
+	int smallShipEX = -1;
+	int smallShipEY = -1;
+
+	while(true)
+	{
+		smallShipEX = rand() % (SIZE);
+		smallShipEY = rand() % (SIZE);
+
+	if (m[smallShipEX][smallShipEY] != WATER_VALUE)
+	{
+		continue;
+	}
+
+	
+
+	m[smallShipEX][smallShipEY] = SMALL_VALUE_E;
+	
+	break;
+	}
+
+
 }
 
 	
