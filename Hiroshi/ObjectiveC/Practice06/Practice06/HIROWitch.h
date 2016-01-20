652804bdd85da46ed2f7d10e5fe63113c9376f1e
Hiroshi Developer <19.hiroshi.13@gmail.com>
2016-01-19 19:46:47 -0800

    create human object

---
+++ Hiroshi/ObjectiveC/Practice06/Practice06/HIROWitch.h
@@ -0,0 +1,13 @@
+//
+//  HIROWitch.h
+//  Practice06
+//
+//  Created by Hiroshi Tokutomi on 2016-01-19.
+//  Copyright © 2016 Hiroshi Tokutomi. All rights reserved.
+//
+
+#import "HIROHuman.h"
+
+@interface HIROWitch : HIROHuman
+
+@end
---
+++ Hiroshi/ObjectiveC/Practice06/Practice06/HIROWitch.m
@@ -0,0 +1,13 @@
+//
+//  HIROWitch.m
+//  Practice06
+//
+//  Created by Hiroshi Tokutomi on 2016-01-19.
+//  Copyright © 2016 Hiroshi Tokutomi. All rights reserved.
+//
+
+#import "HIROWitch.h"
+
+@implementation HIROWitch
+
+@end
